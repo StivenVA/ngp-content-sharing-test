5f3aa6114d65c6ce3dbdf59b551cfad15307f64c
Stiven Villarraga <stiven.villaraga@sasoftco.com>
2025-11-10 18:25:16 -0500

    first commit

---
+++ src/main/java/com/stivenva/contentsharingtest/ContentSharingTestApplication.java
@@ -0,0 +1,13 @@
+package com.stivenva.contentsharingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSharingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentSharingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stivenva/contentsharingtest/ContentSharingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stivenva.contentsharingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentSharingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
